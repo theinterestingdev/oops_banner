fc49b4035198e534cd557fc714a6da281925eee3
unknown <kartiks04800@gmail.com>
2025-12-29 12:14:38 +0530

    Encapsulating_the_data

--- oops/src/OOPSBannerApp.java
+++ oops/src/OOPSBannerApp.java
@@ -1,45 +1,165 @@
 public class OOPSBannerApp {
-    public static String[] getOPattern()
+
+    /**
+     * Character Pattern Map - Inner class for storing character to pattern mappings
+     */
+
+    static class CharacterPatternMap{
+        /**
+         * Character Representation
+         */
+        private final char character;
+
+        /**
+        Pattern for printing the characters
+         */
+        private final String[] pattern;
+
+        // Constructor
+
+        public CharacterPatternMap(char character,String[] pattern)
+        {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter for Character
+        public char getCharacter(){
+            return character;
+        }
+        public String[] getPattern()
+        {
+            return pattern;
+        }
+    }
+
+    // Class Variable
+
+    private static CharacterPatternMap[] patterns;
+
+    // Pattern Initialization
+
+    static void patternInit()
     {
-        return new String[]{
-                "   ***   ",
-                "**     **",
-                "**     **",
-                "**     **",
-                "**     **",
-                "**     **",
-                "   ***   "
+        patterns = new CharacterPatternMap[]{
+                new CharacterPatternMap('O',new String[]{
+                        "   ***   ",
+                        "**     **",
+                        "**     **",
+                        "**     **",
+                        "**     **",
+                        "**     **",
+                        "   ***   "
+                }),
+                new CharacterPatternMap('P',new String[]{
+                        "*******  ",
+                        "**    ** ",
+                        "**    ** ",
+                        "*******  ",
+                        "**       ",
+                        "**       ",
+                        "**       "
+                }),
+                new CharacterPatternMap('S',new String[]{
+                        "  ******  ",
+                        " **       ",
+                        " **       ",
+                         "  *****  ",
+                        "       ** ",
+                        "       ** ",
+                        " ******   "
+                }),
+
         };
     }
 
-    public static String[] getPPattern()
+    // Getting the pattern
+
+    static String[] getPattern(char ch)
     {
-        return new String[]{
-                "*******  ",
-                "**    ** ",
-                "**    ** ",
-                "*******  ",
-                "**       ",
-                "**       ",
-                "**       "
-        };
+        for(CharacterPatternMap cpm : patterns)
+        {
+            if(cpm.getCharacter() == ch)
+            {
+                return cpm.getPattern();
+            }
+        }
+
+        return null;
     }
 
-    public static String[] getSPattern()
+    // Print the banner
+
+    static void printBanner()
     {
-        return new String[]{
-                "  ****** ",
-                " **       ",
-                " **       ",
-                "  *****  ",
-                "       ** ",
-                "       ** ",
-                " ******  "
-        };
+        String[] oPattern = getPattern('O');
+        String[] pPattern = getPattern('P');
+        String[] sPattern = getPattern('S');
+
+        String[] banner = new String[7];
+
+        for(int i = 0;i< oPattern.length;i++)
+        {
+            banner[i] = String.join("  ",oPattern[i],oPattern[i],pPattern[i],sPattern[i]);
+        }
+
+        for(String line : banner)
+        {
+            System.out.println(line);
+        }
+
     }
+
+
+
+
+
+//    public static String[] getOPattern()
+//    {
+//        return new String[]{
+//                "   ***   ",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "   ***   "
+//        };
+//    }
+//
+//    public static String[] getPPattern()
+//    {
+//        return new String[]{
+//                "*******  ",
+//                "**    ** ",
+//                "**    ** ",
+//                "*******  ",
+//                "**       ",
+//                "**       ",
+//                "**       "
+//        };
+//    }
+//
+//    public static String[] getSPattern()
+//    {
+//        return new String[]{
+//                "  ****** ",
+//                " **       ",
+//                " **       ",
+//                "  *****  ",
+//                "       ** ",
+//                "       ** ",
+//                " ******  "
+//        };
+//    }
 
 
     public static void main(String[] args) {
+
+        // calling the methods
+
+        patternInit();
+        printBanner();
 
         // Display the literal text "OOPS"
 
@@ -93,27 +213,23 @@ public class OOPSBannerApp {
 
         // Improving modularity and printing into reusable functions
 
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
-
-        String[] banner = new String[7];
-
-        for(int i = 0;i< oPattern.length;i++)
-        {
-            banner[i] = String.join("  ",oPattern[i],oPattern[i],pPattern[i],sPattern[i]);
-        }
+//        String[] oPattern = getOPattern();
+//        String[] pPattern = getPPattern();
+//        String[] sPattern = getSPattern();
+//
+//        String[] banner = new String[7];
+//
+//        for(int i = 0;i< oPattern.length;i++)
+//        {
+//            banner[i] = String.join("  ",oPattern[i],oPattern[i],pPattern[i],sPattern[i]);
+//        }
 
         // Print the banner
-
-        for(String line : banner)
-        {
-            System.out.println(line);
-        }
-
-
-
-
+//
+//        for(String line : banner)
+//        {
+//            System.out.println(line);
+//        }
 
     }
 
