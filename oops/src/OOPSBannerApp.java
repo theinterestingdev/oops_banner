b465e8e2fe9e7f69bf737c5066f48cf648879ed5
unknown <kartiks04800@gmail.com>
2025-12-29 10:58:34 +0530

    improving_modularity

--- oops/src/OOPSBannerApp.java
+++ oops/src/OOPSBannerApp.java
@@ -1,4 +1,44 @@
 public class OOPSBannerApp {
+    public static String[] getOPattern()
+    {
+        return new String[]{
+                "   ***   ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "   ***   "
+        };
+    }
+
+    public static String[] getPPattern()
+    {
+        return new String[]{
+                "*******  ",
+                "**    ** ",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    public static String[] getSPattern()
+    {
+        return new String[]{
+                "  ****** ",
+                " **       ",
+                " **       ",
+                "  *****  ",
+                "       ** ",
+                "       ** ",
+                " ******  "
+        };
+    }
+
+
     public static void main(String[] args) {
 
         // Display the literal text "OOPS"
@@ -8,48 +48,72 @@ public class OOPSBannerApp {
 
         // Render OOPS as Banner (spaces and *)
 
-        String[] O = {
-                "   ***   ",
-                "**     **",
-                "**     **",
-                "**     **",
-                "**     **",
-                "**     **",
-                "   ***   "
-        };
+//        String[] O = {
+//                "   ***   ",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "**     **",
+//                "   ***   "
+//        };
+//
+//        String[] P = {
+//                "*******  ",
+//                "**    ** ",
+//                "**    ** ",
+//                "*******  ",
+//                "**       ",
+//                "**       ",
+//                "**       "
+//        };
+//
+//
+//        String[] S = {
+//                "  ****** ",
+//                " **       ",
+//                " **       ",
+//                "  *****  ",
+//                "       ** ",
+//                "       ** ",
+//                " ******  "
+//        };
+//
+//
+//
+//        String[] banner = new String[7];
+//
+//        for (int i = 0; i < 7; i++) {
+//            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+//        }
+//
+//        for (String line : banner) {
+//            System.out.println(line);
+//        }
 
-        String[] P = {
-                "*******  ",
-                "**    ** ",
-                "**    ** ",
-                "*******  ",
-                "**       ",
-                "**       ",
-                "**       "
-        };
+        // Improving modularity and printing into reusable functions
 
-
-        String[] S = {
-                "  ****** ",
-                " **       ",
-                " **       ",
-                "  *****  ",
-                "       ** ",
-                "       ** ",
-                " ******  "
-        };
-
-
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
         String[] banner = new String[7];
 
-        for (int i = 0; i < 7; i++) {
-            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        for(int i = 0;i< oPattern.length;i++)
+        {
+            banner[i] = String.join("  ",oPattern[i],oPattern[i],pPattern[i],sPattern[i]);
         }
 
-        for (String line : banner) {
+        // Print the banner
+
+        for(String line : banner)
+        {
             System.out.println(line);
         }
+
+
+
+
 
     }
 
