b9104078c8cef69dd1de2a4ee9c24cee0c810952
unknown <kartiks04800@gmail.com>
2025-12-29 10:37:15 +0530

    Render_OOPS_As_Large_Banner

--- oops/src/OOPSBannerApp.java
+++ oops/src/OOPSBannerApp.java
@@ -3,7 +3,55 @@ public class OOPSBannerApp {
 
         // Display the literal text "OOPS"
 
-        System.out.println("OOPS");
-        
+        // System.out.println("OOPS");
+
+
+        // Render OOPS as Banner (spaces and *)
+
+        String[] O = {
+                "   ***   ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "   ***   "
+        };
+
+        String[] P = {
+                "*******  ",
+                "**    ** ",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+
+
+        String[] S = {
+                "  ****** ",
+                " **       ",
+                " **       ",
+                "  *****  ",
+                "       ** ",
+                "       ** ",
+                " ******  "
+        };
+
+
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+
     }
+
+
 }
